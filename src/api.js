a7265a3e4c765839003b297ab55c8dd193d9f05f
filipsuk <filipsuk@gmail.com>
2017-03-29 14:31:35 +0200

    fetch data from apiary mock server

--- src/api.js
+++ src/api.js
@@ -1,17 +1,28 @@
 /* @flow */
-import type { EventsData } from './types/apiData';
-
 import { mockedEvents } from './mocks/mockedEvents';
 
+import type { EventsData } from './types/apiData';
+
+const host = 'https://private-6e9cf6-eventigo.apiary-mock.com/';
+const useLocalMock = false;
+
 function fetchApiEvents(): Promise<EventsData> {
+  if (useLocalMock) {
+    return localMock();
+  }
+
+  return fetch(`${host}api/v1/events`).then(response => response.json());
+}
+
+const localMock = () => {
   return new Promise((resolve, reject) => {
     setTimeout(
       () => {
         return resolve(mockedEvents);
       },
-      2000
+      1000
     );
   });
-}
+};
 
 export { fetchApiEvents };
