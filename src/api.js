b613f8652ab97ca4fa900dab5b2efdf4ee772f9d
filipsuk <filipsuk@gmail.com>
2017-04-26 18:10:41 +0200

    🔧 install axios

--- src/api.js
+++ src/api.js
@@ -1,10 +1,14 @@
 /* @flow */
+import axios from 'axios';
 import type { EventsData } from './types/apiData';
+import type { $AxiosXHR } from 'axios';
 
 const host = 'https://private-6e9cf6-eventigo.apiary-mock.com/';
 
 function fetchApiEvents(): Promise<EventsData> {
-  return fetch(`${host}api/v1/events`).then(response => response.json());
+  return axios
+    .get(`${host}api/v1/events`)
+    .then((response: $AxiosXHR<EventsData>) => response.data);
 }
 
 export { fetchApiEvents };
