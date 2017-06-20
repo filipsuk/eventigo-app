5ff37ad1984b05b8d2d660a2c54f1fc70db0a1f5
filipsuk <filipsuk@gmail.com>
2017-06-20 21:55:55 +0200

    ✨ persist redux store

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,6 +1,8 @@
 /* @flow */
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore, autoRehydrate } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
 import reducers from './reducers';
 import { createEpicMiddleware } from 'redux-observable';
 import { fetchEventsEpic } from './epics';
@@ -10,10 +12,15 @@ const enhancers = composeWithDevTools(
   {
     // Options: https://github.com/zalmoxisus/redux-devtools-extension/blob/master/docs/API/Arguments.md
   }
-)(applyMiddleware(...middlewares));
+)(applyMiddleware(...middlewares), autoRehydrate());
 
 export default function configureStore(initialState: any = {}) {
   const store = createStore(reducers, initialState, enhancers);
+
+  // Get stored state, fire a rehydrate action, and begin persisting redux state
+  persistStore(store, { storage: AsyncStorage }, () => {
+    console.log('Redux store restored');
+  });
 
   // Allow reducers hot reloading
   if (module.hot) {
