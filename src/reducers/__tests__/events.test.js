7b711d7507763c3a38c5569acfeaf13b05e125e9
filipsuk <filipsuk@gmail.com>
2017-06-22 13:07:14 +0200

    🚨 add events reducer test

--- src/reducers/__tests__/events.test.js
+++ src/reducers/__tests__/events.test.js
@@ -2,10 +2,12 @@
 
 import eventsReducer from '../events';
 import { mockedEvents } from '../../__mocks__/data/mockedEvents';
+import moment from 'moment';
 
-import type { EventsState } from '../events';
+import type { EventsState, EventsStateData } from '../events';
 import type { EventsData } from '../../types/apiData';
 import type { Action } from '../../types/actions';
+import type { Event } from '../../types/model';
 
 describe('events reducer', () => {
   const initialState: EventsState = {
@@ -38,6 +40,61 @@ describe('events reducer', () => {
 
     expect(eventsReducer(undefined, action)).toMatchObject({
       dataFetched: true,
+      isFetching: false,
+      error: false
+    });
+  });
+
+  it('filters past events', () => {
+    jest.unmock('moment');
+    const pastEvent: Event = {
+      id: '1',
+      name: 'Past event',
+      description: null,
+      url: 'https://example.org',
+      start: moment().subtract(2, 'days').toISOString(),
+      image: 'https://example.org'
+    };
+    const currentEvent: Event = {
+      id: '2',
+      name: 'Current event',
+      description: null,
+      url: 'https://example.org',
+      start: moment().subtract(1, 'hours').toISOString(),
+      end: moment().add(1, 'hours').toISOString(),
+      image: 'https://example.org'
+    };
+    const futureEvent: Event = {
+      id: '3',
+      name: 'Future event',
+      description: null,
+      url: 'https://example.org',
+      start: moment().add(2, 'days').toISOString(),
+      image: 'https://example.org'
+    };
+    const action: Action = {
+      type: 'persist/REHYDRATE',
+      payload: {
+        events: {
+          data: {
+            '1': pastEvent,
+            '2': currentEvent,
+            '3': futureEvent
+          },
+          dataFetched: false,
+          isFetching: false,
+          error: false
+        },
+        bookmarks: {}
+      }
+    };
+
+    expect(eventsReducer(undefined, action)).toEqual({
+      data: {
+        '2': currentEvent,
+        '3': futureEvent
+      },
+      dataFetched: false,
       isFetching: false,
       error: false
     });
